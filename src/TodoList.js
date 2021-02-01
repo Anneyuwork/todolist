d179859aef739cf72718461965d21e9f4dd63b74
rongyu <anne.yu.work@gmail.com>
2021-01-31 23:56:05 -0800

    Input upated in state

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,13 +7,20 @@ class TodoList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = store.getState();
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleStoreChange = this.handleStoreChange.bind(this);
+		store.subscribe(this.handleStoreChange);//making sure component was updated
 
 	}
 	render(){
 		return (
 			<div style = {{marginTop:'10px', marginLeft:'10px'}}>
 				<div>
-					<Input value = {this.state.inputValue} placeholder = 'todo info' style = {{width:'300px'}}/>
+					<Input value = {this.state.inputValue} 
+						placeholder = 'todo info' 
+						style = {{width:'300px'}}
+						onChange={this.handleInputChange}
+					/>
 					<Button type = "primary">Submit</Button>
 				</div>
 				<List
@@ -24,6 +31,18 @@ class TodoList extends Component {
 				/>
 			</div>
 		)
+	}
+
+	handleInputChange(e){
+		const action = {
+			type:'change_input_value',
+			value: e.target.value
+		}
+		store.dispatch(action);
+	}
+
+	handleStoreChange(){
+		this.setState(store.getState());
 	}
 }
 export default TodoList;
