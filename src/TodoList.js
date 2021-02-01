a2e82c3a5b659bc701cd1643c55271fe1ecb79ef
rongyu <anne.yu.work@gmail.com>
2021-01-31 23:37:39 -0800

    add redux, init page infprmation loading

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List } from 'antd';
+import store from './store';
 
-const data = [
-	'Racing car',
-	'Vancouver',
-];
 class TodoList extends Component {
+	constructor(props) {
+		super(props);
+		this.state = store.getState();
+
+	}
 	render(){
 		return (
 			<div style = {{marginTop:'10px', marginLeft:'10px'}}>
 				<div>
-					<Input placeholder = 'todo info' style = {{width:'300px'}}/>
+					<Input value = {this.state.inputValue} placeholder = 'todo info' style = {{width:'300px'}}/>
 					<Button type = "primary">Submit</Button>
 				</div>
 				<List
 					style = {{marginTop:'10px', width:'300px'}}
 					bordered
-					dataSource={data}
+					dataSource={this.state.list}
 					renderItem={item => (<List.Item>{item}</List.Item>)}
 				/>
 			</div>
